aee77703780ea50b5490c8fcbd812fafb04418d9
Volodymyr Bushko <Volodymyr.Bushko@gmail.com>
2020-05-08 18:45:20 +0300

    Fix music keys

--- Piano/MainWindow.xaml.cs
+++ Piano/MainWindow.xaml.cs
@@ -1,43 +1,22 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System;
 using System.Media;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Resources;
 
 namespace Piano
 {
     public partial class MainWindow : Window
     {
-        private List<string> sounds;
-        private MediaPlayer mediaPlayer;
+        private SoundPlayer player = null;
+        private StreamResourceInfo sri = null;
+        private string numberKey = string.Empty;
 
         public MainWindow()
         {
+            player = new SoundPlayer();
+
             InitializeComponent();
-            InitializeSounds();
-        }
-
-        private void InitializeSounds()
-        {
-            sounds = new List<string>();
-            mediaPlayer = new MediaPlayer();
-            DirectoryInfo directory = new DirectoryInfo("Sounds");
-
-            foreach (var file in directory.GetFiles())
-                sounds.Add(file.FullName);
         }
 
         //Андрій, він твій
@@ -52,22 +31,25 @@ namespace Piano
 
         }
 
-
-        //Він мій
         private void button_Click(object sender, RoutedEventArgs e)
         {
             switch ((e.Source as Button).Name)
             {
-                case "button1": mediaPlayer.Open(new Uri(sounds[0])); break;
-                case "button2": mediaPlayer.Open(new Uri(sounds[1])); break;
-                case "button3": mediaPlayer.Open(new Uri(sounds[2])); break;
-                case "button4": mediaPlayer.Open(new Uri(sounds[3])); break;
-                case "button5": mediaPlayer.Open(new Uri(sounds[4])); break;
-                case "button6": mediaPlayer.Open(new Uri(sounds[5])); break;
-                case "button7": mediaPlayer.Open(new Uri(sounds[6])); break;
+                case "button1": numberKey = "1"; break;
+                case "button2": numberKey = "2"; break;
+                case "button3": numberKey = "3"; break;
+                case "button4": numberKey = "4"; break;
+                case "button5": numberKey = "5"; break;
+                case "button6": numberKey = "6"; break;
+                case "button7": numberKey = "7"; break;
             }
 
-            mediaPlayer.Play();
+            sri = Application.GetResourceStream(new Uri("Sounds/soundKey" + numberKey + ".wav", UriKind.Relative));
+
+            player.Stream = sri.Stream;
+            player.Load();
+            player.Play();
+            player.Dispose();
         }
     }
 }
