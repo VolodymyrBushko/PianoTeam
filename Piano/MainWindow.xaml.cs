5243b71cbcd56ecb5a0e12539b983edf74e9d8b9
Volodymyr Bushko <Volodymyr.Bushko@gmail.com>
2020-05-08 14:50:14 +0300

    Add sounds for music keys

--- Piano/MainWindow.xaml.cs
+++ Piano/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Media;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,9 +21,23 @@ namespace Piano
 {
     public partial class MainWindow : Window
     {
+        private List<string> sounds;
+        private MediaPlayer mediaPlayer;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeSounds();
+        }
+
+        private void InitializeSounds()
+        {
+            sounds = new List<string>();
+            mediaPlayer = new MediaPlayer();
+            DirectoryInfo directory = new DirectoryInfo("Sounds");
+
+            foreach (var file in directory.GetFiles())
+                sounds.Add(file.FullName);
         }
 
         //Андрій, він твій
@@ -35,12 +53,21 @@ namespace Piano
         }
 
 
-
-
         //Він мій
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show((e.Source as Button).Content.ToString());
+            switch ((e.Source as Button).Name)
+            {
+                case "button1": mediaPlayer.Open(new Uri(sounds[0])); break;
+                case "button2": mediaPlayer.Open(new Uri(sounds[1])); break;
+                case "button3": mediaPlayer.Open(new Uri(sounds[2])); break;
+                case "button4": mediaPlayer.Open(new Uri(sounds[3])); break;
+                case "button5": mediaPlayer.Open(new Uri(sounds[4])); break;
+                case "button6": mediaPlayer.Open(new Uri(sounds[5])); break;
+                case "button7": mediaPlayer.Open(new Uri(sounds[6])); break;
+            }
+
+            mediaPlayer.Play();
         }
     }
 }
