436366bcfff897d15e9a517d1f64252070472099
Volodymyr Bushko <Volodymyr.Bushko@gmail.com>
2020-05-10 17:56:36 +0300

    Set color Blue

--- Piano/MainWindow.xaml.cs
+++ Piano/MainWindow.xaml.cs
@@ -19,13 +19,13 @@ namespace Piano
             InitializeComponent();
         }
 
-        //Андрій, він твій
+        //Руслан
         private void buttonPlay_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        //Андрій, він твій
+        //Андрый
         private void buttonStartStop_Click(object sender, RoutedEventArgs e)
         {
 
